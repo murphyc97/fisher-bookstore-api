f1a14f7600a2456fea5b460f021fbe5c46cd5688
Connor Murphy <murphyc97@gmail.com>
2020-04-08 23:30:13 -0400

    init the changes

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,6 @@ namespace Fisher.Bookstore
                 options.UseNpgsql(Configuration.GetConnectionString("BookstoreContext"))
             );
             services.AddScoped<IBooksRepository, BooksRepository>();
-            services.AddSingleton<IBooksRepository, TestBooksRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
